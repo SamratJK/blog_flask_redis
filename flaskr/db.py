3cdbb243936d80c9cbf3e5c7aa9ec265d1fd6eb4
Samrat <thapa_samrat1@outlook.com>
2022-07-09 23:06:11 +0545

    made docker-compose

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,18 +1,19 @@
-# import sqlite3
-import redis 
+import sqlite3
+
 import click 
 from flask import current_app,g
 from flask.cli import with_appcontext 
+import redis
 
 def get_db():
     if 'db' not in g:
-        g.db = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
-
         # g.db = sqlite3.connect(
         #         current_app.config['DATABASE'],
         #         detect_types = sqlite3.PARSE_DECLTYPES
         # )
         # g.db.row_factory = sqlite3.Row
+
+        g.db = redis.StrictRedis(host='redis', port=6379, charset="utf-8", decode_responses=True)
 
     return g.db
     
@@ -26,8 +27,8 @@ def close_db(e=None):
 def init_db():
     db = get_db()
 
-    # with current_app.open_resource('schema.sql') as f:
-    #     db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
